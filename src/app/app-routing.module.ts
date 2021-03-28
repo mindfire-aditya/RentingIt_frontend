b9879741d86b8d2d52be20727337b04515c0aa7d
Aditya Kumar Sahu <77772688+mindfire-aditya@users.noreply.github.com>
2021-03-28 15:38:07 +0530

    routes and components
    Components - Home, About, ProductCategories, Login, Register, Navbar, Footer, ProductDetails, ProductItem, ProductList, SearchBar
    
    Routes - home, about, categories(all, bikes, cars, electronics, appliances, furnitures, machines), product/product-details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,81 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductCategoriesComponent } from './product-categories/product-categories.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'categories',
+    children: [
+      {
+        path: 'appliances',
+        component: ProductListComponent,
+      },
+      {
+        path: 'bikes',
+        component: ProductListComponent,
+      },
+      {
+        path: 'cars',
+        component: ProductListComponent,
+      },
+      {
+        path: 'electronics',
+        component: ProductListComponent,
+      },
+      {
+        path: 'furnitures',
+        component: ProductListComponent,
+      },
+      {
+        path: 'machines',
+        component: ProductListComponent,
+      },
+      {
+        path: 'all',
+        component: ProductCategoriesComponent,
+      },
+    ],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'product',
+    children: [
+      {
+        path: 'product-details',
+        component: ProductDetailsComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ProductCategoriesComponent } from './product-categories/product-categories.component';
+import { CategoryItemComponent } from './product-categories/category-item/category-item.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductItemComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+    SearchBarComponent,
+    ProductCategoriesComponent,
+    CategoryItemComponent,
+    AboutComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
