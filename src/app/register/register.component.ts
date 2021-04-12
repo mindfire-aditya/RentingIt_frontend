6850dcf31a2ab8f2dfeadad93225770a65c81af2
Aditya Kumar Sahu <77772688+mindfire-aditya@users.noreply.github.com>
2021-04-12 10:37:16 +0530

    closing braces

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,48 +4,47 @@ import { SignupService } from '../services/signupService/signup.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  //defining my custom userdetails object
+  userDetails = {
+    username: '',
+    email: '',
+    password: '',
+    role: ['user'],
+  };
 
-//defining my custom userdetails object
-userDetails = {
-  username:'',
-  email:'',
-  password:'',
-  role:["user"]
-}
+  constructor(private signup: SignupService) {}
+  ngOnInit(): void {}
 
-constructor(private signup:SignupService) { }
-ngOnInit(): void {}
+  //function for registering the user
+  registerUser() {
+    //console.log("Form is submited!!");
+    if (
+      this.userDetails.username != '' &&
+      this.userDetails.email != '' &&
+      this.userDetails.password != '' &&
+      this.userDetails.username != null &&
+      this.userDetails.email != '' &&
+      this.userDetails.password != null
+    ) {
+      //we have to submit the form
 
-//function for registering the user
-registerUser()
-{
-  //console.log("Form is submited!!");
-  if((this.userDetails.username!='' && this.userDetails.email!='' && this.userDetails.password!='') && (this.userDetails.username!=null&& this.userDetails.email!='' && this.userDetails.password!=null))
-  {
-    //we have to submit the form
-
-    console.log("Here form will be submited") 
-    this.signup.addUser(this.userDetails).subscribe
-    (
-      (response:any)=>{
-      //success
-      console.log(response);
-      window.location.href="/login";
-      },
-      (error:any)=>{
-        //error
-        console.log(error);
-      }
-      
-    ) 
+      console.log('Here form will be submited');
+      this.signup.addUser(this.userDetails).subscribe(
+        (response: any) => {
+          //success
+          console.log(response);
+          window.location.href = '/login';
+        },
+        (error: any) => {
+          //error
+          console.log(error);
+        }
+      );
+    } else {
+      console.log('Fields are empty !!');
+    }
   }
-  else
-  {
-    console.log("Fields are empty !!");
-  }
-
-
 }
