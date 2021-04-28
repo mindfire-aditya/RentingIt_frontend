d7343c1cb3564133ba542c2719a774937628d7d4
Aditya Kumar Sahu <77772688+mindfire-aditya@users.noreply.github.com>
2021-04-28 19:45:48 +0530

    added form in rent component and order model

---
+++ src/app/models/order.ts
@@ -0,0 +1,9 @@
+export class Order {
+  constructor(
+    public rent_mode: string,
+    public units: number,
+    public start_datetime: Date,
+    public end_datetime: Date,
+    public terms_and_conditions: boolean
+  ) {}
+}
--- src/app/rent/rent.component.ts
+++ src/app/rent/rent.component.ts
@@ -2,6 +2,7 @@
  * @author Aditya Sahu
  */
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../models/order';
 
 @Component({
   selector: 'app-rent',
@@ -9,7 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rent.component.css'],
 })
 export class RentComponent implements OnInit {
+  newOrder = new Order('', 1, new Date(), new Date(), false);
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    let rent_mode = document.getElementById('rent_mode');
+    console.log(rent_mode);
+    console.log('Hello');
+  }
+
+  onSelect() {
+    console.log('REnt mode');
+  }
 }
