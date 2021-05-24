872ea9c74f1125c2dfc5ec0ad4135b48761c06b8
Aditya Kumar Sahu <77772688+mindfire-aditya@users.noreply.github.com>
2021-05-24 22:22:39 +0530

    Revert "Merge branch 'Feature-b' into dev"
    This reverts commit 918b8af9a0dcee167a658c316437775235d7b374, reversing
    changes made to c3f35c42fba4979948297b0de9d5d2a2e01d9a99.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,42 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
-import { ProductItemComponent } from './components/product-item/product-item.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 import { ProductCategoriesComponent } from './product-categories/product-categories.component';
 import { CategoryItemComponent } from './product-categories/category-item/category-item.component';
-import { AboutComponent } from './components/about/about.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { ProfilePageComponent } from './components/profile-page/profile-page.component';
-import { MyOrdersComponent } from './components/my-orders/my-orders.component';
-import { MyProductsComponent } from './components/my-products/my-products.component';
-import { AddEditProductComponent } from './components/add-edit-product/add-edit-product.component';
-import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
-import { ErrorPageComponent } from './components/error-page/error-page.component';
-import { RentComponent } from './components/rent/rent.component';
-import { OrderItemComponent } from './components/order-item/order-item.component';
-import { OrderDetailsComponent } from './components/order-details/order-details.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { MyProductsComponent } from './my-products/my-products.component';
+import { AddEditProductComponent } from './add-edit-product/add-edit-product.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { RentComponent } from './rent/rent.component';
+import { OrderItemComponent } from './order-item/order-item.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokenInterceptorService } from './services/interceptor/token-interceptor/token-interceptor.service';
 import { AuthGuard } from './services/AuthGuard/auth.guard';
-import { NavbarLoggedinComponent } from './components/navbar-loggedin/navbar-loggedin.component';
-import { PlaceOrderComponent } from './components/place-order/place-order.component';
+import { NavbarLoggedinComponent } from './navbar-loggedin/navbar-loggedin.component';
+import { TestComponent } from './test/test.component';
+import { PlaceOrderComponent } from './place-order/place-order.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ProductSubCategoryComponent } from './components/product-sub-category/product-sub-category.component';
-import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
-import { MyProductsOnRentComponent } from './components/my-products-on-rent/my-products-on-rent.component';
+import { ProductSubCategoryComponent } from './product-sub-category/product-sub-category.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { MyProductsOnRentComponent } from './my-products-on-rent/my-products-on-rent.component';
 
 @NgModule({
   declarations: [
@@ -66,9 +68,11 @@ import { MyProductsOnRentComponent } from './components/my-products-on-rent/my-p
     OrderItemComponent,
     OrderDetailsComponent,
     NavbarLoggedinComponent,
+    TestComponent,
     PlaceOrderComponent,
     ProductSubCategoryComponent,
     LoadingSpinnerComponent,
+    CheckoutComponent,
     MyProductsOnRentComponent,
   ],
   imports: [
---
+++ src/app/place-order/place-order.component.ts
@@ -0,0 +1,302 @@
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Order } from '../models/order';
+import { PlaceOrderService } from '../services/placeOrder/place-order.service';
+import { ProductService } from '../services/products/product.service';
+import { UserDetailService } from '../services/userDetail/user-detail.service';
+import { render } from 'creditcardpayments/creditCardPayments';
+import { Product } from '../models/product';
+import { MyOrder } from '../models/my-order';
+import { MyOrdersService } from '../services/my-orders/my-orders.service';
+
+@Component({
+  selector: 'app-place-order',
+  templateUrl: './place-order.component.html',
+  styleUrls: ['./place-order.component.css'],
+})
+export class PlaceOrderComponent implements OnInit {
+  ordersListByProductId: MyOrder[] = [];
+  newOrder: Order;
+  product_item: Product;
+  pickup_address: any;
+  actualName: any;
+  ownerId: any;
+  customerId = localStorage.getItem('id');
+  imageBaseUrl =
+    'http://localhost:8080/rentingIt/product/resources/download-image/';
+
+  today: string;
+  public image: any;
+  isAvailable: boolean = true;
+  showText: boolean = false;
+  private subscription1: Subscription = new Subscription();
+  private subscription2: Subscription = new Subscription();
+  private subscription3: Subscription = new Subscription();
+  private subscription4: Subscription = new Subscription();
+
+  /**
+   *
+   * @param activatedRoute
+   * @param productService
+   * @param userDetailService
+   * @param placeOrderService
+   * @param myOrdersService
+   * @param router
+   */
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService,
+    private userDetailService: UserDetailService,
+    private placeOrderService: PlaceOrderService,
+    private myOrdersService: MyOrdersService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.today = new Date().toISOString();
+    let rem = this.today.split(':');
+    rem.pop();
+    this.today = rem.join(':');
+
+    let productId = this.activatedRoute.snapshot.paramMap.get('productId');
+    this.newOrder = new Order(0, 0, 0, '', 1, new Date(), new Date(), true, 0);
+
+    let customerId = Number(localStorage.getItem('id'));
+
+    this.newOrder.customerId = customerId;
+
+    if (productId !== null) {
+      this.subscription1 = this.productService
+        .getProductById(parseInt(productId))
+        .subscribe(
+          (data) => {
+            this.product_item = data;
+
+            this.ownerId = this.product_item.ownerId;
+            this.actualName = this.product_item.actualName;
+            this.getOwnerInfo(this.ownerId);
+            this.product_item.imageUrl =
+              this.imageBaseUrl + this.product_item.imageUrl;
+            this.newOrder.ownerId = this.ownerId;
+            this.newOrder.productId = this.product_item.id;
+          },
+          (error) => console.log(error)
+        );
+    } else {
+      console.log('Null Id: ', productId);
+    }
+
+    //placed orders of the product to check availibilty
+    this.subscription2 = this.myOrdersService
+      .getOrdersByProductId(Number(productId))
+      .subscribe((data) => {
+        this.ordersListByProductId = data;
+      });
+  }
+
+  /**
+   *
+   * @param ownerId
+   */
+  getOwnerInfo(ownerId: number) {
+    if (ownerId) {
+      this.subscription3 = this.userDetailService
+        .getOwnerDetail(this.ownerId)
+        .subscribe(
+          (data) => {
+            this.pickup_address = data;
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    }
+  }
+
+  /**
+   *
+   * @param start
+   * @param end
+   */
+  calculateTotalPrice(start: Date, end: Date) {
+    if (
+      this.checkProductAvailibilty(
+        this.newOrder.start_datetime,
+        this.newOrder.end_datetime
+      )
+    ) {
+      this.showText = true;
+      this.isAvailable = true;
+      let diff = this.dateTimeDifference(start, end);
+
+      let rent_mode = this.newOrder.rent_mode;
+      this.newOrder.total_amount = 0;
+      let price = 0;
+      switch (rent_mode) {
+        case 'per hour':
+          price =
+            this.newOrder.units * diff.hours * this.product_item.pricePerHour;
+          if (price <= 0) {
+            alert('Please select appropriate Start and End datetime.');
+            this.newOrder.total_amount = 0;
+          } else {
+            this.newOrder.total_amount =
+              this.newOrder.units * diff.hours * this.product_item.pricePerHour;
+          }
+
+          break;
+
+        case 'per day':
+          price =
+            this.newOrder.units * diff.hours * this.product_item.pricePerHour;
+          if (price <= 0) {
+            alert('Please select appropriate Start and End datetime.');
+            this.newOrder.total_amount = 0;
+          } else {
+            this.newOrder.total_amount =
+              this.newOrder.units * diff.days * this.product_item.pricePerDay;
+          }
+
+          break;
+
+        case 'per week':
+          price =
+            this.newOrder.units * diff.hours * this.product_item.pricePerHour;
+          if (price <= 0) {
+            alert('Please select appropriate Start and End datetime.');
+            this.newOrder.total_amount = 0;
+          } else {
+            this.newOrder.total_amount =
+              this.newOrder.units * diff.weeks * this.product_item.pricePerWeek;
+          }
+
+          break;
+
+        case 'per month':
+          price =
+            this.newOrder.units * diff.hours * this.product_item.pricePerHour;
+          if (price <= 0) {
+            alert('Please select appropriate Start and End datetime.');
+            this.newOrder.total_amount = 0;
+          } else {
+            this.newOrder.total_amount =
+              this.newOrder.units *
+              diff.months *
+              this.product_item.pricePerMonth;
+          }
+          break;
+        default:
+      }
+
+      render({
+        id: '#myPaypalButtons',
+        currency: 'IN',
+        value: this.newOrder.total_amount.toString(),
+        onApprove: (details) => {
+          alert('Transection Successfull!!');
+          this.onSubmit();
+        },
+      });
+    } else {
+      this.showText = true;
+      this.isAvailable = false;
+    }
+  }
+
+  /**
+   *
+   * @param start
+   * @param end
+   * @returns
+   */
+  dateTimeDifference(start: Date, end: Date) {
+    start = new Date(start);
+    end = new Date(end);
+
+    let msInDay = 1000 * 3600 * 24;
+
+    let timeDiff = end.getTime() - start.getTime();
+
+    let days = Math.ceil(timeDiff / msInDay);
+    let hours = Math.ceil(timeDiff / (1000 * 3600));
+    let weeks = Math.floor(days / 7);
+    let months = Math.floor(days / 30);
+
+    return {
+      days: days,
+      months: months,
+      hours: hours,
+      weeks: weeks,
+    };
+  }
+
+  /**
+   *
+   */
+  onSubmit() {
+    if (
+      this.newOrder.rent_mode != '' &&
+      this.newOrder.rent_mode != null &&
+      this.newOrder.terms_and_conditions != false
+    ) {
+      if (this.isAvailable) {
+        this.subscription4 = this.placeOrderService
+          .addOder(this.newOrder)
+          .subscribe(
+            (data) => {
+              console.log(data);
+              this.router.navigate(['user/my-orders']);
+            },
+            (error) => {
+              error(error);
+            }
+          );
+      } else {
+        this.isAvailable = false;
+      }
+    } else {
+      alert('Please fill all the fields');
+    }
+  }
+
+  /**
+   *
+   * @param start
+   * @param end
+   * @returns
+   */
+  checkProductAvailibilty(start: Date, end: Date): boolean {
+    start = new Date(start);
+    end = new Date(end);
+    let startTimeInMs = start.getTime();
+    let endTimeInMs = end.getTime();
+
+    for (let i in this.ordersListByProductId) {
+      if (
+        new Date(this.ordersListByProductId[i].rentEndDate).getTime() -
+          startTimeInMs >=
+        0
+      ) {
+        return false;
+      }
+      if (
+        endTimeInMs -
+          new Date(this.ordersListByProductId[i].rentStartDate).getTime() <=
+        0
+      ) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  ngOnDestroy() {
+    this.subscription1.unsubscribe();
+    this.subscription2.unsubscribe();
+    this.subscription3.unsubscribe();
+    this.subscription4.unsubscribe();
+  }
+}
